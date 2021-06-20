ac3c1dc63231e589d316357232df05cf4a265bb2
Kamil Matula <43967269+MKamil99@users.noreply.github.com>
2021-06-20 14:54:33 +0200

    Fixed GPS-Permission error
    Removed redundant code from MainActivity - location doesn't need to be gathered after changing permissions. This action also eliminated bug in which application was shutting down.

--- app/src/main/java/com/example/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.weatherapp.view
 
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.ActivityMainBinding
-import com.example.weatherapp.viewmodel.LocationVM
 import com.example.weatherapp.viewmodel.WeatherVM
 
 class MainActivity : AppCompatActivity() {
@@ -19,9 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
         // Gathering data from Room:
         weatherVM = ViewModelProvider(this).get(WeatherVM::class.java)
         weatherVM.weatherInfo.observe(this, {
@@ -36,15 +29,5 @@ class MainActivity : AppCompatActivity() {
             if (it != null && weatherVM.currentDailyForecast.value == null)
                 weatherVM.setDailyForecastByRoomData(it)
         })
-
-        return super.onCreateView(name, context, attrs)
-    }
-
-    // Reaction for granting GPS permissions:
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 2 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            ViewModelProvider(this).get(LocationVM::class.java).launchGPS(this)
-        }
     }
 }
